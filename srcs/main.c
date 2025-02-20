e393c5b2caff518777524382105c41eab5a358e6
croueAlec <alec.croue@gmail.com>
2025-02-20 16:02:20 +0100

    added set_hash_type function

--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,25 @@
 #include "ft_ssl.h"
 
-int main(int argc, char const *argv[])
+t_hash_type	set_hash_type(const char *cmd)
 {
+	if (!cmd)
+		return (INVALID);
+
+	if (strcmp(cmd, "md5") == 0)
+		return (MD5);
+
+	if (strcmp(cmd, "sha256") == 0)
+		return (SHA256);
+
+	return (INVALID);
+}
+
+int main(__attribute__((unused)) int argc, char const *argv[])
+{
+	t_ssl	ssl = {0};
+
+	ssl.hash_type = set_hash_type(argv[1]);
+	printf("%d\n", ssl.hash_type);
+
 	return 0;
 }
