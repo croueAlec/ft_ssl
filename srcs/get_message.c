672ac846305344a06c5e1dd33531413420a02236
croueAlec <alec.croue@gmail.com>
2025-02-27 15:04:08 +0100

    added infile reading

--- srcs/get_message.c
+++ srcs/get_message.c
@@ -2,16 +2,32 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-char	*open_infile(t_hash *node, t_ssl *ssl)
+static char	*read_file(int fd)
+{
+	char	*tmp = get_next_line(fd);
+	char	*file_str = NULL;
+	if (!tmp)
+		return (NULL);
+	while (tmp)
+	{
+		file_str = ft_sep_join(file_str, tmp, NULL);
+		tmp = NULL;
+		if (!file_str)
+			return (NULL);
+		tmp = get_next_line(fd);
+	}
+	close(fd);
+	return (file_str);
+}
+
+static char	*open_infile(t_hash *node, t_ssl *ssl)
 {
 	int fd = open(node->file, O_RDONLY);
 	if (fd < 0) {
 		error_open_infile(ssl);
 		return (NULL);
-	} else
-	{
-		close(fd);
-		return (NULL);
+	} else {
+		return (read_file(fd));
 	}
 }
 
@@ -28,7 +44,7 @@ char	*get_message(t_hash *node, t_ssl *ssl)
 		break;
 
 	case INFILE:
-		return (node->file);
+		return (open_infile(node, ssl));
 		break;
 	}
 }
