d7bddb00a2008c8e728388999d35077f977e2ac7
croueAlec <alec.croue.pro@gmail.com>
2025-10-17 14:33:44 +0200

    added colors to block_print

--- test_md5/md5_blocks_utils.c
+++ test_md5/md5_blocks_utils.c
@@ -14,19 +14,35 @@ static uint32_t	uint8_to_uint32(uint8_t *buf)
 	return val;
 }
 
+static void	set_byte_color(size_t separator_index, size_t current_index)
+{
+	p_print_trace("\033[0m");
+	if (separator_index == current_index) {
+		p_print_trace("\033[1;33m");
+	} else if (current_index >= PADDED_CHUNK_SIZE && separator_index < PADDED_CHUNK_SIZE)
+	{
+		p_print_trace("\033[0;94m");
+	}
+	else if (current_index < separator_index)
+	{
+		p_print_trace("\033[0;32m");
+	}
+}
+
 /**
  * @brief Prints only on TRACE log_level
  */
-void	print_block_chunk(t_block *block)
+void	print_block_chunk(t_block *block, size_t separator_index)
 {
 	if (!level_verification(P_LOG_TRACE))
 		return ;
 
 	for (size_t i = 0; i < 64; i++)
 	{
+		set_byte_color(separator_index, i);
 		print_chunk_uint8_t((uint8_t)(block->chunk[i]));
 		if ((i+1) % 8 == 0) {
-			p_print_trace("\t   %08x  %08x\n", uint8_to_uint32(&block->chunk[i-7]), uint8_to_uint32(&block->chunk[i-3]));
+			p_print_trace("\033[0;1m\t   0x%08x  0x%08x\033[0m\n", uint8_to_uint32(&block->chunk[i-7]), uint8_to_uint32(&block->chunk[i-3]));
 		}
 		else if (i != 63)
 			p_print_trace(" ");
