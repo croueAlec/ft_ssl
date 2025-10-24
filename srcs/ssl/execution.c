croueAlec/ft_ssl

commit 495f8726d4cff002e17fb06f85b382d92d900f06

feat: change MAX_HASH_NBR interpretation for clarity

srcs/ssl/execution.c
@@ -5,12 +5,12 @@ static void	define_algorithm_array(hash_func_array *algorithm_array)
 	algorithm_array[INVALID] = NULL;
 	algorithm_array[MD5] = md5;
 	algorithm_array[SHA256] = sha256;
-	algorithm_array[MAX_HASH_NBR - 1] = NULL;
+	algorithm_array[MAX_HASH_NBR] = NULL;
 }
 
 bool	execution(t_ssl *ssl)
 {
-	hash_func_array	algorithm[MAX_HASH_NBR] = {0};
+	hash_func_array	algorithm[MAX_HASH_NBR + 2] = {0};	// Adding 2 for INVALID add NULL terminating.
 	define_algorithm_array(algorithm);
 
 	t_hash	*tmp = NULL, *node = ssl->hash_list;

includes/ft_hash_list.h
@@ -23,7 +23,7 @@
 # define SHA256_index 0
 #endif
 
-#define MAX_HASH_NBR 2 +2 // Always add +2 to the hash_number to account for INVALID and the NULL terminating
+#define MAX_HASH_NBR 2	// Increment MAX_HASH_NBR when adding a new hash
 typedef void	(*hash_func_array)(t_hash*);
 typedef enum	e_hash_types
 {

commit b399e936a7527b20b4baf574a6370e93e584a9e9

fix (ssl): Fix segfault introduced in commit 495f872

srcs/ssl/execution.c
@@ -5,7 +5,7 @@ static void	define_algorithm_array(hash_func_array *algorithm_array)
 	algorithm_array[INVALID] = NULL;
 	algorithm_array[MD5] = md5;
 	algorithm_array[SHA256] = sha256;
-	algorithm_array[MAX_HASH_NBR] = NULL;
+	algorithm_array[MAX_HASH_NBR + 2] = NULL;
 }
 
 bool	execution(t_ssl *ssl)
@@ -15,6 +15,8 @@ bool	execution(t_ssl *ssl)
 
 	t_hash	*tmp = NULL, *node = ssl->hash_list;
 	size_t	node_count = 0;
+	p_print_info("%d\n", ssl->hash_type);
+
 	while (node)
 	{
 		tmp = node->next;